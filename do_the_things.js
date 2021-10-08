7bc9198b1ed3125d6e5d332c3b08305ee32cca3b
newbster88 <jtnewby88@gmail.com>
2021-10-08 17:57:04 -0400

    seems to be working

---
+++ do_the_things.js
@@ -0,0 +1,42 @@
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function get_dem_stonks() {
+    await sleep(2000);
+
+    var rows = document.querySelectorAll('a.row__StyledGridTableRow-ncfewc-0.duTlLi');
+    var stonks = []
+
+    // get and store dem stonks into a dictionary along with the amount of shares
+    rows.forEach(function (item, index) {
+        var stock = item.children[0].children[0].children[1].children[0].children[0].children[0].textContent;
+        var amount = item.children[1].children[0].textContent
+        var price = item.children[2].children[0].textContent
+        price = price.substring(1);
+        stonks.push([stock, amount, price])
+    });
+
+    // Start the CSV stuffs
+    //define the heading for each row of the data  
+    var csv = 'ticker,shares,avg_price\n';
+
+    //merge the data with CSV  
+    stonks.forEach(function (row) {
+        csv += row.join(',');
+        csv += "\n";
+    });
+
+    // stuff needed to download csv
+    var hiddenElement = document.createElement('a');
+    hiddenElement.href = 'data:text/csv;charset=utf-8,' + encodeURI(csv);
+    hiddenElement.target = '_blank';
+
+    //provide the name for the CSV file to be downloaded  
+    hiddenElement.download = 'M1_Stonks.csv';
+    hiddenElement.click();
+
+}
+
+
+get_dem_stonks();
